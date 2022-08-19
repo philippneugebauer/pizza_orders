ca97c7c8896714b1696e8203be0d176e79c6a37d
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-08-19 14:50:12 +0200

    add seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ActiveRecord::Base.transaction do
+  small_pizza = PizzaSize.create! name: "Small", multiplier: 0.7
+  PizzaSize.create! name: "Medium", multiplier: 1
+  PizzaSize.create! name: "Large", multiplier: 1.3
+
+  Pizza.create! name: "Margherita", price: 5
+  salami_pizza = Pizza.create! name: "Salami", price: 6
+  Pizza.create! name: "Tonno", price: 8
+
+  Ingredient.create! name: "Onions", price: 1
+  Ingredient.create! name: "Cheese", price: 2
+  Ingredient.create! name: "Olives", price: 2.5
+
+  Promotion.create! code: "2FOR1", pizza_id: salami_pizza.id, pizza_size_id: small_pizza.id, from: 2, to: 1
+
+  Discount.create! code: "SAVE5", percentage: 5
+end
