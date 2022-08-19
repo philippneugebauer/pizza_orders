021d428c96e1a54c40160b040d50ada99ca8d44e
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-08-19 14:43:53 +0200

    add discount computations to discount model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,4 +4,12 @@ class Discount < ApplicationRecord
   validates :code, presence: true
   validates :percentage, numericality: { greater_than: 0, less_than: 100 }
   validates :code, uniqueness: true
+
+  def discount_price(price)
+    price - compute_discount_sum(price)
+  end
+
+  def compute_discount_sum(price)
+    price * percentage / 100
+  end
 end
