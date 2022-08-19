6489ba8bb2c461f89b7aa7a24e8f4e999714e4d9
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-08-19 14:52:15 +0200

    add order system test

---
+++ test/system/orders_test.rb
@@ -0,0 +1,20 @@
+require "application_system_test_case"
+
+class OrdersTest < ApplicationSystemTestCase
+  setup do
+    @order = orders(:one)
+  end
+
+  test "visiting the index" do
+    visit orders_url
+    assert_selector "h1", text: "Orders"
+  end
+
+  test "should complete order" do
+    visit orders_url
+    click_on "Complete", match: :first
+
+    assert_text "Order completed."
+  end
+
+end
