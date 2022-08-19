af7f289a36170af2a631a4a834e5df2b76d4e6ce
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-08-19 14:43:15 +0200

    revise validations in oder items and promotions

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -5,7 +5,8 @@ class Promotion < ApplicationRecord
   belongs_to :pizza_size
   belongs_to :pizza
 
-  validates :code, :pizza, :pizza_size, :to, presence: true
+  validates :code, :pizza, :pizza_size, presence: true
   validates :from, numericality: { only_integer: true, greater_than: 0 }
+  validates :to, numericality: { only_integer: true, greater_than_or_equal: 0 }
   validates :code, uniqueness: true
 end
