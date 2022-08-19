43b08378e5f09a69bab76f8ee89b50d71886bbdd
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-08-19 14:44:12 +0200

    add order item price computations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -8,4 +8,16 @@ class OrderItem < ApplicationRecord
   has_many :omited, -> { where(kind: "omited") }, class_name: 'OrderItemIngredient'
 
   validates :order, :pizza, :pizza_size, presence: true
+
+  def compute_price
+    compute_pizza_price() + compute_extras_price()
+  end
+
+  def compute_pizza_price
+    pizza.price * pizza_size.multiplier
+  end
+
+  def compute_extras_price
+    extras.map {|e| e.ingredient.price}.sum() * pizza_size.multiplier
+  end
 end
